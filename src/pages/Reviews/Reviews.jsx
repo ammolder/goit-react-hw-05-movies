94cefe652183cfff4fb2df575ee0a9fe314ba590
ammolder <98980721+ammolder@users.noreply.github.com>
2022-11-29 11:31:06 +0100

    Update Reviews.jsx

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getMovies } from 'api/getMovies';
+import { getMovies } from '../../api/getMovies';
 import { ReviewsList } from 'components/ReviewsList/ReviewsList';
 
 const Reviews = () => {
